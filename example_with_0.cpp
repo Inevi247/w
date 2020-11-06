dca68abe57b77fded256f55625f021da818a8f22
Eugene Anfalov <anfalovd10@yandex.ru>
2020-11-06 09:22:06 +0500

    Create example_with_0.cpp

---
+++ example_with_0.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+//c++; ++c; = c + 1;
+//c--; --c; c = c - 1;
+
+int main()
+{
+	int a, c = 0;
+	cin >> a;
+	while (a != 0)
+	{
+		c++; // c = c + 1;
+		cin >> a;
+	}
+	cout << c;
+}
